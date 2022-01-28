6af0829a72ac1eacabdeaaacb6da5f4de0309a07
Michele Cappannari <michele.cappannari@keysoluzioni.it>
2022-01-28 12:22:27 +0100

    ok

--- resources/views/backend/livewire/fields-livewire.blade.php
+++ resources/views/backend/livewire/fields-livewire.blade.php
@@ -9,7 +9,7 @@
                 
                 <style>
                     #mapindex {
-                        height: 600px;
+                        height: 400px;
                         /* The height is 400 pixels */
                         width: 100%;
                         /* The width is the width of the web page */
