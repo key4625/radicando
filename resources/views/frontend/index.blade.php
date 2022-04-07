9ad2812943f0c2c81a06fddf83753325ca37cb61
Michele Cappannari <michele.cappannari@keysoluzioni.it>
2022-04-07 15:50:23 +0200

    oknuovo

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,7 +3,7 @@
 @section('Ordina', __('Ordina'))
 
 @section('content')
-<img src="{{ Storage::url(App\Models\Setting::find('app_img_copertina')->value)}}">
+<img class="w-100" src="{{ Storage::url(App\Models\Setting::find('app_img_copertina')->value)}}">
 <div class="container">
     <h1>Benvenuto</h1>
 </div>
