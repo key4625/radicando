b893ae876a2a6fdbdaa47f55a3cce603705b0a1c
Michele Cappannari <michele.cappannari@keysoluzioni.it>
2022-06-23 15:53:49 +0200

    stampa ordine

--- resources/views/backend/livewire/order/print.blade.php
+++ resources/views/backend/livewire/order/print.blade.php
@@ -25,73 +25,75 @@
 		</div>
 	@endisset
 
-    <div id="dastampare" class="row" style="display:none;">
-        @foreach($orders->get() as $order)
-        <div class="col-6">
-            <h3 class="d-inline">{{$order->nome}} {{$order->cognome}}</h3>
-            <h5  class="d-inline">{{Carbon\Carbon::create($order->data)->translatedFormat('D d M')}} </h5>
-            <p>Tel. {{$order->telefono}} - Indirizzo {{$order->citta}} {{$order->indirizzo}}</p>
-            @if($order->plants()->count()>0)
-                <table class="table table-sm table-bordered w-full">
-                    <thead>
-                        <tr><td colspan="4" class="text-bold">Frutta e verdura</td></tr>
-                        <tr>
-                            <td>Nome</td>
-                            <td>Quantità</td>
-                            <td>Prezzo</td>
-                            <td>Totale</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($order->plants()->withPivot('quantity','quantity_um','price','price_um')->orderby('fragile','desc')->get() as $tmp_item_order)
-                            <tr> 
-                                <td>{{$tmp_item_order->nome}}</td>
-                                <td>{{ $tmp_item_order->pivot->quantity}} {{ $tmp_item_order->pivot->quantity_um}}</td>
-                                <td>{{$tmp_item_order->pivot->price}}€ / {{$tmp_item_order->pivot->price_um}}</td>
-                                <td>@if($tmp_item_order->pivot->price_um==$tmp_item_order->pivot->quantity_um) 
-                                        {{$tmp_item_order->pivot->price * $tmp_item_order->pivot->quantity }} €
-                                    @else 
-                                        &nbsp;
-                                    @endif 
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            @endif
-            
-            @if($order->products()->count()>0)
-                <table class="table table-sm table-bordered w-full">
-                    <thead>
-                        <tr><td colspan="4" class="text-bold">Prodotti</td></tr>
-                        <tr>
-                            <td>Nome</td>
-                            <td>Quantità</td>
-                            <td>Prezzo</td>
-                            <td>Totale</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($order->products()->withPivot('quantity','quantity_um','price','price_um')->orderby('fragile','desc')->get() as $tmp_item_order)
-                            <tr> 
-                                <td>{{$tmp_item_order->name}}</td>
-                                <td>{{ $tmp_item_order->pivot->quantity}} {{ $tmp_item_order->pivot->quantity_um}}</td>
-                                <td>{{$tmp_item_order->pivot->price}}€ / {{$tmp_item_order->pivot->price_um}}</td>
-                                <td>
-                                    @if($tmp_item_order->pivot->price_um==$tmp_item_order->pivot->quantity_um) 
-                                        {{$tmp_item_order->pivot->price * $tmp_item_order->pivot->quantity }} €
-                                    @else 
-                                        &nbsp;
-                                    @endif 
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            @endif  
-            <h5>TOTALE: {{$order->prezzo_tot}} €</h5>      
-            <hr>     
-        @endforeach
+    <div id="dastampare" style="display:block;">
+        <div class="row">
+            @foreach($orders->get() as $order)
+                <div class="col-6">
+                    <h3 class="d-inline">{{$order->nome}} {{$order->cognome}}</h3>
+                    <h5  class="d-inline">{{Carbon\Carbon::create($order->data)->translatedFormat('D d M')}} </h5>
+                    <p>Tel. {{$order->telefono}} - Indirizzo {{$order->citta}} {{$order->indirizzo}}</p>
+                    @if($order->plants()->count()>0)
+                        <table class="table table-sm table-bordered w-full">
+                            <thead>
+                                <tr><td colspan="4" class="text-bold">Frutta e verdura</td></tr>
+                                <tr>
+                                    <td>Nome</td>
+                                    <td>Quantità</td>
+                                    <td>Prezzo</td>
+                                    <td>Totale</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($order->plants()->withPivot('quantity','quantity_um','price','price_um')->orderby('fragile','desc')->get() as $tmp_item_order)
+                                    <tr> 
+                                        <td>{{$tmp_item_order->nome}}</td>
+                                        <td>{{ $tmp_item_order->pivot->quantity}} {{ $tmp_item_order->pivot->quantity_um}}</td>
+                                        <td>{{$tmp_item_order->pivot->price}}€ / {{$tmp_item_order->pivot->price_um}}</td>
+                                        <td>@if($tmp_item_order->pivot->price_um==$tmp_item_order->pivot->quantity_um) 
+                                                {{$tmp_item_order->pivot->price * $tmp_item_order->pivot->quantity }} €
+                                            @else 
+                                                &nbsp;
+                                            @endif 
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endif
+                    
+                    @if($order->products()->count()>0)
+                        <table class="table table-sm table-bordered w-full">
+                            <thead>
+                                <tr><td colspan="4" class="text-bold">Prodotti</td></tr>
+                                <tr>
+                                    <td>Nome</td>
+                                    <td>Quantità</td>
+                                    <td>Prezzo</td>
+                                    <td>Totale</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($order->products()->withPivot('quantity','quantity_um','price','price_um')->orderby('fragile','desc')->get() as $tmp_item_order)
+                                    <tr> 
+                                        <td>{{$tmp_item_order->name}}</td>
+                                        <td>{{ $tmp_item_order->pivot->quantity}} {{ $tmp_item_order->pivot->quantity_um}}</td>
+                                        <td>{{$tmp_item_order->pivot->price}}€ / {{$tmp_item_order->pivot->price_um}}</td>
+                                        <td>
+                                            @if($tmp_item_order->pivot->price_um==$tmp_item_order->pivot->quantity_um) 
+                                                {{$tmp_item_order->pivot->price * $tmp_item_order->pivot->quantity }} €
+                                            @else 
+                                                &nbsp;
+                                            @endif 
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endif  
+                    <h5>TOTALE: {{$order->prezzo_tot}} €</h5>      
+                    <hr>
+                </div>     
+            @endforeach
         </div>
     </div>
 
