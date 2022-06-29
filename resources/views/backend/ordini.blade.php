5cd8f695922fc4a0188b2516bb96230207f412d8
Michele Cappannari <michele.cappannari@keysoluzioni.it>
2022-06-29 11:23:51 +0200

    file stampa

--- resources/views/backend/ordini.blade.php
+++ resources/views/backend/ordini.blade.php
@@ -2,6 +2,18 @@
 
 @section('title', __('Ordini'))
 
+@push('after-styles')
+<style>
+@media print
+{
+  table { page-break-after:auto }
+  tr    { page-break-inside:avoid; page-break-after:auto }
+  td    { page-break-inside:avoid; page-break-after:auto }
+  thead { display:table-header-group }
+  tfoot { display:table-footer-group }
+}
+</style>
+@endpush
 
 
 @section('content')
