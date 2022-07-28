d42c15310d3de539876050d71e0cb853ee041bde
Michele Cappannari <michele.cappannari@keysoluzioni.it>
2022-07-28 16:55:03 +0200

    okok

--- resources/views/landlord/layouts/app.blade.php
+++ resources/views/landlord/layouts/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ appName() }} | @yield('title')</title>
-    <meta name="description" content="@yield('meta_description', appName())">
+    <title>Radicando | Agricoltura online</title>
+    <meta name="description" content="Gestire la tua azienda agricola non è mai stato così semplice!">
     <meta name="author" content="@yield('meta_author', 'Anthony Rappa')">
     @yield('meta')
 
